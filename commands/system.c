cbad66bade827a8eecee28bf3ec6af0908629c74
james.sanders1331 <james.sanders1337@gmail.com>
2022-08-21 20:13:38 +1000

    Add OS specific print_sys

--- commands/system.c
+++ commands/system.c
@@ -11,6 +11,7 @@
 #include <errno.h>
 #include <libgen.h>
 
+
 #include "system.h"
 void get_cpu() // Uses uts api to get CPU
 {
@@ -19,20 +20,28 @@ void get_cpu() // Uses uts api to get CPU
     printf("System is %s on %s hardware\n", uts.sysname, uts.machine);
     printf("OS Release is %s\n", uts.release);
     printf("OS Version is %s\n", uts.version);
+    
+
+    printf("CPU INFO:\n");
+    
+    
 }
 
 void print_sys() // prints version of OS and CPU type
 {
 #if __APPLE__
+    system("sysctl -a | grep machdep.cpu | grep machdep.cpu.brand_string"); // CPU INFO FOR MAC
     printf("This is a MAC_OS operating system\n");
 #elif _WIN32
     printf("This is a Windows operating system\n");
 #elif _WIN64
     printf("This is a Windows 64bit operating system");
 #elif __LINUX__
+    system("less /proc/cpuinfo");
     printf("This is a Linux operating system\n");
 #elif _POSIX_VERSION
     printf("This is a POSIX based operating system (Windows with cygwin)\n");
+    system("less /proc/cpuinfo");
 #else
     printf("Operating system not detected.\n");
 #endif
